3bfb408f3f0b0c12ad166d3873e73aa6da439dfa
mulengasampa3 <mulengasampa3@gmail.com>
2023-08-17 12:28:40 +0200

    Entities
    Mulenga Sampa: Created Some Project Entities, Awaiting Review Boss

---
+++ Entities.sql
@@ -0,0 +1,36 @@
+1. STAFF
+  -Staff_ID
+  -NRC 
+  -SfName
+  -SlName
+  -DOB
+  -Employment_date
+  -SERVICE Attributes(Service_Type)
+  -current_status
+  -SPhone_ID
+  -Location
+  
+  
+2. CUSTOMER(END USER)
+  -Customer_ID
+  -CfName
+  -ClName
+  -CPhone_ID
+  -email
+  -Date_Booked
+  -STAFF Attributes (Location, SName, SPhone_ID)
+  
+  
+3. SERVICE
+  -STAFF Attributes(SName, )
+  -Service_ID
+  -Service_Type
+  -Price
+  
+  
+4. LOCATION
+  -City
+  -Town
+  -Country
+  -Country_Code
+  -Coordinate
